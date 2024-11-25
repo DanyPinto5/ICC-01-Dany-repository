59dcb675452495611f8ccdba255cc7d2f7554f6b
Dany <danybpinto98@gmail.com>
2024-11-25 00:52:04 +0000

    Add new file

--- user_activity_log.sh
+++ user_activity_log.sh
@@ -7,6 +7,6 @@ username=$(whoami)
 log_file="user_activity.log"
 custom_message="Already log."
 echo "$current_date_time - $username: $custom_message" >> "$log_file" 
-echo "this will be appended to the file" >> user_activity.log
+echo "this will be appended to the file" >> user_activity_log.txt
 
 
