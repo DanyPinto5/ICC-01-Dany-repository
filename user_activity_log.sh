7cd7415c0afb7ab37228262565379659ee82fa78
Dany <danybpinto98@gmail.com>
2024-11-25 00:40:10 +0000

    event logs

---
+++ user_activity_log.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_date_time=$(date +"%y-%m-%d %H:%M:%S")
+
+username=$(whoami)
+
+log_file="user_activity.log"
+custom_message="Already log."
+echo "$current_date_time - $username: $custom_message" >> "$log_file" 
+echo "this will be appended to the file" >> user_activity.log
+
+
